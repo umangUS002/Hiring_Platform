db1bd11c9f46a41de52ce385e9a9c7cce6393c5b
Umang Srivastava <btech10271.23@bitmesra.ac.in_umangUS002>
2026-02-16 00:33:43 +0530

    dss

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,6 +1,6 @@
 import ImageKit from "@imagekit/nodejs";
 
-var imagekit = new ImageKit.default({
+var imagekit = new ImageKit({
     publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
